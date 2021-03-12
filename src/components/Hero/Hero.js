f3463effbd281400bdda401491ba463f14996619
Joz-Bruer Quince <quincejozbruer@gmail.com>
2021-03-11 23:25:11 -0800

    Creating the Hero

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import './Hero.css'
+import img from './img.svg'
+import img2 from './img2.svg'
+
+function Hero() {
+    return (
+        <div className='Hero'>
+            <div className='img1'>
+                <img src={img} alt="Donuts"/>
+            </div>
+            <div className='Hero-Img'>
+
+            </div>
+            <div className='img2'>
+                <img src={img2} alt="Sphere"/>
+            </div>
+        </div>
+    )
+}
+
+// https://i.ibb.co/NZmNh8X/IMG-9601.jpg
+// https://i.ibb.co/yYMKc5W/IMG-9612.jpg
+// https://i.ibb.co/VBBw7J2/IMG-9658.jpg
+// https://i.ibb.co/Zx8FsD0/IMG-9657.jpg
+// https://i.ibb.co/xjSBDBz/IMG-9643.jpg
+// https://i.ibb.co/YdCdZJY/IMG-9640.jpg
+// https://i.ibb.co/0Mg69JM/IMG-9620.jpg
+// https://i.ibb.co/VY9RR2x/IMG-9659.jpg
+// https://i.ibb.co/FXKQh0v/IMG-9660-1.jpg
+// https://i.ibb.co/G3Fxswj/IMG-9671.jpg
+// https://i.ibb.co/R4CkGWp/IMG-9675.jpg
+// https://i.ibb.co/PrWXCzQ/IMG-9680-1.jpg
+// https://i.ibb.co/xHbBxrR/IMG-9681-1.jpg
+export default Hero
