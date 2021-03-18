f7fecf1cc8f086ab5d12a1006ffcc81dce2d977c
Joz-Bruer Quince <quincejozbruer@gmail.com>
2021-03-17 23:49:46 -0700

    Working on the slider

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import Hero from "./components/Hero/Hero";
-import Slider from "./components/Slider/Slider";
+import Sliders from "./components/Slider/Slider";
 import Images from './data/data'
 
 
 function App() {
+  
   return (
     <>
       <Hero image={Images[0].url} />
-      <Slider />
+      <Sliders images={Images} />
     </>
   );
 }
--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,12 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Slider from "react-slick";
 import './Slider.css'
 
-function Slider() {
+function Sliders(props) {
+    props.images.shift()
+
+    const [imageIndex, setImageIndex] = useState(1)
+    
+    var settings = {
+        dots: true,
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      speed: 2000,
+      autoplaySpeed: 2000,
+      };
+     
     return (
-        <div className='Slider'>
-            
+        <div className='Slider-Box'>
+            <div className='slider__images__box'>
+                <Slider {...settings}>
+
+                    {props.images.map((img, idx) => 
+                        <div className={idx === imageIndex ? 'slide ActiveSlide' : 'slide'}>
+                            <img src={img.url} alt={img.name}/>
+                        </div>
+                    )}
+                </Slider>
+            </div>
         </div>
     )
 }
 
-export default Slider
+export default Sliders
