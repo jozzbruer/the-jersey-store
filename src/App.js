e42eb2e2c707ed3f4ea563d766625a771e1ea9d3
Joz-Bruer Quince <quincejozbruer@gmail.com>
2021-03-19 00:13:28 -0700

    Done with the unresponsive middle part

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Hero from "./components/Hero/Hero";
+import Middle from "./components/Middle/Middle";
 import Sliders from "./components/Slider/Slider";
 import Images from './data/data'
 
@@ -9,6 +10,7 @@ function App() {
     <>
       <Hero image={Images[0].url} />
       <Sliders images={Images} />
+      <Middle />
     </>
   );
 }
---
+++ src/components/Middle/Middle.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Middle.css'
+
+function Middle(props) {
+    return (
+        <div className='middle-container'>
+            <div className="pic1"></div>
+            <div className="pic2"></div>
+        </div>
+    )
+}
+
+export default Middle
